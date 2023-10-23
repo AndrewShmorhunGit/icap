414f853ca4979e9c86a7a91410b196933b2b56b1
Andrew <andrewshmorhun@gmail.com>
2023-10-23 14:36:15 +0300

    initial commit

---
+++ src/app/(auth)/register/[[...register]]/page.tsx
@@ -0,0 +1,13 @@
+import { SignUp } from "@clerk/nextjs";
+
+export default function RegisterPage() {
+  return (
+    <SignUp
+      path="/register"
+      routing="path"
+      signInUrl="/login"
+      redirectUrl="/auth"
+      afterSignUpUrl="/auth"
+    />
+  );
+}
